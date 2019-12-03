149dd421f277bcaa31357b25f0a685e773619905
Jan Dudulski <jan@dudulski.pl>
2019-12-03 10:36:22 +0100

    Init day 3

---
+++ day03/lib/day03.ex
@@ -0,0 +1,19 @@
+defmodule Day03 do
+  @moduledoc """
+  Documentation for Day03.
+  """
+
+  @doc """
+  Answer part 1.
+  """
+  def part1 do
+    # todo
+  end
+
+  @doc """
+  Answer part 2.
+  """
+  def part2 do
+    # todo
+  end
+end
