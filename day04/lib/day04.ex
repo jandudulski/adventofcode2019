1d8f73485dd821fa5bd0bc735b9101a5fb569c9f
Jan Dudulski <jan@dudulski.pl>
2019-12-04 19:49:59 +0100

    Init day 4

---
+++ day04/lib/day04.ex
@@ -0,0 +1,19 @@
+defmodule Day04 do
+  @moduledoc """
+  Documentation for Day04.
+  """
+
+  @doc """
+  Answer part 1.
+  """
+  def part1 do
+    # todo
+  end
+
+  @doc """
+  Answer part 2.
+  """
+  def part2 do
+    # todo
+  end
+end
